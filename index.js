d75e7add2f5e93c8aae2ade6e705ff1f4e0953c9
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-29 13:38:31 +0600

    added update reuest api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2qwdips.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,9 @@ async function run() {
     const DistrictCollection = client.db("Area").collection("District");
     const UpazilaCollection = client.db("Area").collection("Upazila");
     const UsersCollection = client.db("Blood-Donation").collection("users");
+    const requestsCollection = client
+      .db("Blood-Donation")
+      .collection("requests");
 
     // get area
     app.get("/districts", async (req, res) => {
@@ -64,33 +67,90 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/user',async(req,res)=>{
+    app.patch("/user", async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
       }
-   
 
       const profile = req.body;
 
       const updatedProfile = {
         $set: {
           name: profile.name,
-          avatarImage : profile.avatar,
+          avatarImage: profile.avatar,
           bloodGroup: profile.bloodGroup,
-          upazila:profile.upazila,
-          district:profile.district,
-         
-          
-
+          upazila: profile.upazila,
+          district: profile.district,
         },
       };
 
-      console.log(profile)
-
       const result = await UsersCollection.updateOne(query, updatedProfile);
       res.send(result);
-    })
+    });
+
+    // request donation related api
+
+    app.post("/request", async (req, res) => {
+      const request = req.body;
+      const result = await requestsCollection.insertOne(request);
+      res.send(result);
+    });
+
+    app.get("/request", async (req, res) => {
+      const result = await requestsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/currentUserRequests", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = requestsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/request/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await requestsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/request/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await requestsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/request/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const body = req.body;
+      const updatedRequest = {
+        $set: {
+          recipientName: body.recipientName,
+          requiredBloodGroup: body.requiredBloodGroup,
+          upazila: body.upazila,
+          district: body.district,
+          hospitalName: body.hospitalName,
+          fullAddress: body.fullAddress,
+          donationDate: body.donationDate,
+          donationTime: body.donationTime,
+          message: body.message,
+        },
+      };
+      const result = await requestsCollection.updateOne(query, updatedRequest);
+      res.send(result);
+    });
+
+
+
+    
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
